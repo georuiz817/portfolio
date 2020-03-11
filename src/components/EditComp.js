42dfc1335e3df6b9b3f748d96d61c5e51979fb21
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-03-11 00:28:33 -0400

    font-color added to editor

--- src/components/EditComp.js
+++ src/components/EditComp.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import {Col, Row } from 'react-bootstrap';
-
 const PinkBack = () => {document.getElementsByTagName('body')[0].style.backgroundColor = 'pink'}
 const GreenBack = () => {document.getElementsByTagName('body')[0].style.backgroundColor = '#52ff51'}
 const OrangeBack = () => {document.getElementsByTagName('body')[0].style.backgroundColor = 'orange'}
 const YellowBack = () => {document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow'}
 const BlueBack = () => {document.getElementsByTagName('body')[0].style.backgroundColor = 'blue'}
+
+const PinkFont = () => {document.getElementsByTagName('body')[0].style.color = 'pink'}
+const GreenFont = () => {document.getElementsByTagName('body')[0].style.color = 'green'}
+const OrangeFont = () => {document.getElementsByTagName('body')[0].style.color = 'orange'}
+const YellowFont= () => {document.getElementsByTagName('body')[0].style.color = 'yellow'}
+const BlueFont = () => {document.getElementsByTagName('body')[0].style.color = 'blue'}
 
 const Arial = () =>{document.getElementsByTagName('body')[0].style.fontFamily = 'Arial'}
 const Script = () =>{document.getElementsByTagName('body')[0].style.fontFamily = 'Brush Script MT, Brush Script Std, cursive'}
@@ -13,8 +18,8 @@ const Console = () =>{document.getElementsByTagName('body')[0].style.fontFamily 
 const Impact = () =>{document.getElementsByTagName('body')[0].style.fontFamily = 'Impact, Charcoal, sans-serif'}
 const Cursive = () =>{document.getElementsByTagName('body')[0].style.fontFamily = 'Apple Chancery, cursive'}
 
-const EditComp = () => {
 
+const EditComp = () => {
   return (
       <div className='Edit-Comp'>
           <div className='edit-comp-header'>
@@ -32,10 +37,20 @@ const EditComp = () => {
                 <div onClick={PinkBack} className="pink-square"></div>
                 <div onClick={YellowBack} className='yellow-square'></div>
                 </div>
+
+                <h2>Font Color</h2>
+                <div className='Color-Pal'>
+                <div onClick={BlueFont} className='blue-square'></div>
+                <div onClick={GreenFont} className="green-square"></div>
+                <div onClick={OrangeFont} className="orange-square"></div>
+                <div onClick={PinkFont} className="pink-square"></div>
+                <div onClick={YellowFont} className='yellow-square'></div>
+
+                </div>
+                
               </Col>
 
               
-
               <Col>
                 <h2>Font-Family</h2>
                 <ul>
@@ -51,6 +66,7 @@ const EditComp = () => {
               <p>Refresh to bring changes to default</p>
 
           </div>
+          
     </div>
    )
   }
